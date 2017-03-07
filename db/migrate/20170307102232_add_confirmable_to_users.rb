37cebebdab1c5d7fab78572ab8e52950a37bd8ce
Gaurav Manchanda <gauravmanchanda16@gmail.com>
2017-03-07 19:35:24 +0530

    Fixed confirmable migration

--- db/migrate/20170307102232_add_confirmable_to_users.rb
+++ db/migrate/20170307102232_add_confirmable_to_users.rb
@@ -1,10 +1,16 @@
 class AddConfirmableToUsers < ActiveRecord::Migration[5.0]
-  def self.change
+  def self.up
     change_table(:users) do |t|
       ## Confirmable
       t.string   :confirmation_token
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
     end
+  end
+
+  def self.down
+    # By default, we don't want to make any assumption about how to roll back a migration when your
+    # model already existed. Please edit below which fields you would like to remove in this migration.
+    raise ActiveRecord::IrreversibleMigration
   end
 end
